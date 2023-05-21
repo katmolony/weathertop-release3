455c2074999c6c0ac5ed70b50b7179aa416c8bbd
Kate Molony <katemolony96@gmail.com>
2023-05-21 15:20:56 +0100

    Added method to keep stations in alphabetical order even if not capatalised

--- app/models/Station.java
+++ app/models/Station.java
@@ -163,11 +163,33 @@ public class Station extends Model {
         return station;
     }
 
+    public static String convertToTitleCaseIteratingChars(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
+        StringBuilder converted = new StringBuilder();
+
+        boolean convertNext = true;
+        for (char ch : text.toCharArray()) {
+            if (Character.isSpaceChar(ch)) {
+                convertNext = true;
+            } else if (convertNext) {
+                ch = Character.toTitleCase(ch);
+                convertNext = false;
+            } else {
+                ch = Character.toLowerCase(ch);
+            }
+            converted.append(ch);
+        }
+
+        return converted.toString();
+    }
 
     //getters
 
     public String getStationTitle() {
-        return title;
+        return convertToTitleCaseIteratingChars(title);
     }
 
     public int latestCode() {
